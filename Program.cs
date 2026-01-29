d30c553c5b5c95f94639002d7df581ed277f7f21
AndreasLK <Andreaslkrus@gmail.com>
2026-01-29 10:56:49 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Laegehuset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
